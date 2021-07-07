68fa2d3875355d0065c2fc14a7a4aaf04ceb525d
Mr-Yoshua <32917052+MR-Yosua@users.noreply.github.com>
2021-07-07 16:36:51 -0400

    test control automatica

---
+++ control +camaras.py
@@ -0,0 +1,121 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+import json
+import pygame
+import pygame.camera
+import time
+
+
+def ReadQR():
+    #img = cv2.imread('QR-Images/qrcode1.png')
+    # Establecer lectura por camara
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)  
+    while True:
+        ## mientras se pueda acceder a camara leer codigo QR desde camara, qr data es el contenido pts es para crear un cuadrado que limita el QR que se obtienen las dim desde
+        # los datos de lectura, se utiliza para esta accion numpy
+        success,img = cap.read()
+        for barcode in decode(img):
+            
+            qrdata = barcode.data.decode('utf-8')
+            
+            dataexample = qrdata.replace("'",'"')
+            
+            final_data = json.loads(dataexample)
+           
+
+            nombre_lectura = final_data["nombrePaciente"]
+            box_lectura = final_data["numeroBox"]
+            ruta_lectura = final_data["ruta"]
+            arr_ruta= ruta_lectura.split(',')
+            print("Ruta :" + arr_ruta[0])
+            #Settings para proyectar en pantalla
+            if arr_ruta:
+                desplazar(arr_ruta)
+
+            pts = np.array([barcode.polygon],np.int32)
+            pts = pts.reshape((-1,1,2))
+            cv2.polylines(img,[pts],True,(0,0,255),5)
+            pts2 = barcode.rect
+            #print(pts2[0])
+            cv2.putText(img,('Box : ' + str(box_lectura)),(pts2[0],pts2[1]-60),cv2.FONT_HERSHEY_SIMPLEX,0.9,(57,255,20),2)
+            cv2.putText(img,('Paciente : ' +nombre_lectura),(pts2[0],pts2[1]-20),cv2.FONT_HERSHEY_SIMPLEX,0.9,(57,255,20),2)
+            # return 
+        cv2.imshow('Result',img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            cv2.destroyAllWindows()
+        
+      
+def desplazar(arr_ruta):
+    pygame.init()
+    screen=pygame.display.set_mode((640,380))
+    print(arr_ruta)
+
+    for event in arr_ruta: 
+        if event == 'w':
+            print("Adelante")
+            time.sleep(0.5)
+        
+        elif event == 's':
+            print("Atras")
+            time.sleep(0.5)
+            
+        elif event == 'a':
+            print("Izquierda")
+            time.sleep(0.5)
+            
+        elif 'd':
+            print("Derecha")
+            time.sleep(0.5)
+            
+        elif event.key == pygame.K_SPACE:
+            print("Detener")  
+
+ReadQR();
+
+def controlDistancia():
+    pygame.init()
+    screen = pygame.display.set_mode((800,600))
+    servo_left = Servo(17)
+    servo_right = Servo(18)
+    
+    while True:    
+        for event in pygame.event.get():
+            servo_left.value = -0.13
+            servo_right.value = -0.15
+            if event.type == pygame.QUIT:
+                raise SystemExit
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    servo_left.max()
+                    servo_right.min()
+                    print("Adelante")
+                    sleep(0.5)
+                
+                elif event.key == pygame.K_s:
+                    servo_left.min()
+                    servo_right.max()
+                    print("Atras")
+                    sleep(0.5)
+                    
+                elif event.key == pygame.K_a:
+                    #servo_left.value = -0.15
+                    servo_left.min()
+                    servo_right.min()
+                    print("Izquierda")
+                    sleep(0.5)
+                    
+                elif event.key == pygame.K_d:
+                    #servo_left.value = -0.13
+                    servo_right.max()
+                    servo_left.max()
+                    print("Derecha")
+                    sleep(0.5)
+                    
+                elif event.key == pygame.K_SPACE:
+                    servo_left.value = -0.13
+                    servo_right.value = -0.20
+                    print("Detener")
